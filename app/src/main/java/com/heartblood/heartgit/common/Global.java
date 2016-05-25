2fe17d86ae8427a3f4c0966d6593e3ddb3e6556c
梦靡 <1310451979@qq.com>
2016-05-25 18:45:50 +0800

    项目创建

---
+++ app/src/main/java/com/heartblood/heartgit/common/Global.java
@@ -0,0 +1,12 @@
+package com.heartblood.heartgit.common;
+
+import org.json.JSONObject;
+
+/**
+ * Created by heartblood on 16/5/25.
+ * 保存全局变量及方法
+ */
+public class Global {
+    protected static JSONObject newsData;
+
+}
